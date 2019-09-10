da8973225281a158aa51fea6ceda4b8415cffe3c
unknown <jonathon.winter979@gmail.com>
2019-09-10 19:38:31 +0800

    Task 2 stuff

--- Test.py
+++ Test.py
@@ -14,19 +14,11 @@ if __name__ == '__main__':
    for f in files:
       image = cv2.imread(f);
 
-      gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-      
-      regions,_ = mser.detectRegions(image)
-
-      hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
-      # cv2.polylines(vis, hulls, 1, (0, 255, 0))``
-
-      mask = np.ones(image.shape[:2],dtype="uint8") * [255,255,255]
-
-      cv2.drawContours(mask, hulls, -1, (255,0,0),-1)
+      pts = np.array(eval(args["coords"]), dtype = "float32")
+      warped = four_point_transform(image, pts)
 
       cv2.imshow('image', image)
-      cv2.imshow('mask', mask)
+      cv2.imshow('warped', warped)
       cv2.waitKey(0)
       cv2.destroyAllWindows()
 
