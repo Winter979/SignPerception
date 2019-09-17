b6e99f4553a0eddcc1f1b65e5e9f1e1844eedccd
Zazu <Jonathon.winter979@gmail.com>
2019-09-18 00:27:11 +0800

    Better Number Recognition

---
+++ CreateTemplates.py
@@ -0,0 +1,48 @@
+import cv2
+
+if __name__ == "__main__":
+   image = cv2.imread("characters.png")
+
+   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+   _,th = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY_INV)
+
+   res = cv2.findContours(th, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+   cnts,_ = res if len(res) == 2 else res[1:3]
+
+
+   better = []
+
+   for c in cnts:
+      x,y,w,h = cv2.boundingRect(c)
+
+      # Remove the non numbers
+      if h < 50:
+         continue
+
+      better.append([x,c])
+
+   better = sorted(better, key=lambda x: x[0])
+
+   better = [ii[1] for ii in better]
+
+   val = 0
+
+   for c in better:
+
+      x,y,w,h = cv2.boundingRect(c)
+
+      crop = th[y:y+h,x:x+w]
+      crop = cv2.resize(crop,(50,50))
+
+      cv2.imwrite("templates/{}.jpg".format(val),crop)
+
+      val += 1
+
+      # cv2.imshow("crop",crop)
+      # cv2.waitKey(0)
+      # cv2.drawContours(image, [c], -1,(0,0,255),1)
+
+
+   # cv2.imshow("image",image)
+   # cv2.waitKey(0)
--- Task1.py
+++ Task1.py
@@ -8,8 +8,76 @@ from Settings import Settings as s
 
 from Tools import *
 
+def mse(imageA, imageB):
+	# the 'Mean Squared Error' between the two images is the
+	# sum of the squared difference between the two images;
+	# NOTE: the two images must have the same dimension
+	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+	err /= float(imageA.shape[0] * imageA.shape[1])
+	
+	# return the MSE, the lower the error, the more "similar"
+	# the two images are
+	return err
+
+def Test_Number(number):
+   files = glob.glob("templates/*.jpg")
+
+   guesses = []
+
+   for f in files:
+      mask = cv2.imread(f)
+      mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+
+      val = f[10]
+      score = mse(number,mask)
+      
+      guesses.append([val,score])
+
+
+      # cv2.imshow("number",number)
+      # cv2.imshow("mask",mask)
+      # cv2.waitKey()
+
+   guesses = sorted(guesses, key=lambda x: x[1])
+
+   if guesses[0][1] > 10000:
+      return '?'
+   else:
+      return guesses[0][0]
+
+def Find_Numbers(mask,image):
+   res = cv2.findContours(~mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+   cnts,_ = res if len(res) == 2 else res[1:3]
+
+   letters = ""
+
+   for c in cnts:
+      x,y,w,h = cv2.boundingRect(c)
+      
+      # Get crop and resize
+      crop = image[y:y+h,x:x+w]
+      crop = cv2.resize(crop,(50,50))
+
+      gray,_ = Cvt_All(crop)
+
+      _,test = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+      guess = Test_Number(test)
+
+      if guess != '?':
+         letters += guess
+
+      # cv2.imshow("test",test)
+      # cv2.waitKey(0)
+
+      cv2.rectangle(image, (x-2,y-2),(x+w+2,y+h+2), (0,0,255), 1)
+
+   # cv2.drawContours(image, cnts, -1, (0,255,0),1)
+
+   print(letters)
+
 def Find_Possible(mask):
-   res = cv2.findContours(~mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+   res = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
    cnts,_ = res if len(res) == 2 else res[1:3]
 
    # cnts = imutils.grab_contours(cnts)  
@@ -30,7 +98,8 @@ def Find_Possible(mask):
       if 200 < area  and area2 > 100:
          ratio = h/w
          if 1 < ratio < 4:
-            cv2.drawContours(new,[c],-1,0,-1)
+            if angle < 30 or angle > 60:
+               cv2.drawContours(new,[c],-1,0,-1)
 
    return new
    
@@ -62,12 +131,11 @@ def Defs_Not(mask):
       x,y,w,h = cv2.boundingRect(c)
       area = w*h
 
-      if 200 < area < 2500:
+      if 200 < area < 3000:
          ratio = h/w
 
          if 1.2<ratio < 3:
             cv2.drawContours(new, [c],0,255,-1)
-            print(area)
          else:
             pass
             # cv2.drawContours(new, [c],0,125,-1)
@@ -75,7 +143,7 @@ def Defs_Not(mask):
 
    return new
 
-def Gold_Mask2(hsv,image):
+def Gold_Mask2(hsv):
    
 
    gold = Create_Mask(hsv, "gold")
@@ -84,7 +152,7 @@ def Gold_Mask2(hsv,image):
 
    gold = cv2.medianBlur(gold, 55)
 
-   res = cv2.findContours(gold, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+   res = cv2.findContours(gold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
    cnts,_ = res if len(res) == 2 else res[1:3]
    
    for c in cnts:
@@ -96,28 +164,54 @@ def Gold_Mask2(hsv,image):
          
    return ~gold
 
+def Red_Mask(hsv):
+   
+   red1 = Create_Mask(hsv, "red1")
+   red2 = Create_Mask(hsv, "red2")
+
+   red = red1 + red2
+
+   # Trim_Edges(red,color=0)
+
+   # red = cv2.medianBlur(red, 55)
+
+   # res = cv2.findContours(red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+   # cnts,_ = res if len(res) == 2 else res[1:3]
+   
+   # for c in cnts:
+   #    area = cv2.contourArea(c)
+
+   #    if area < 1000:
+   #       cv2.drawContours(red, [c], 0, 255, -1)
+         # cv2.drawContours(image, [c], 0, (0,0,255), 2)
+         
+   return ~red
+
 def main(files):
    for f in files:
       image = cv2.imread(f)
-      _,hsv = Cvt_All(image)
+
+      blur = cv2.bilateralFilter(image, 15,50,50)
+
+      _,hsv = Cvt_All(blur)
 
       white = Create_Mask(hsv, "white")
 
-
-      mask = Get_Mask(image)
+      mask = Get_Mask(blur)
       maybe = Defs_Not(mask)
 
-      gold = Gold_Mask2(hsv,image)
+      gold = Gold_Mask2(hsv)
+      red = Red_Mask(hsv)
 
       new = cv2.bitwise_and(maybe,white)
       new = cv2.bitwise_and(new,new,mask=gold)
+      new = cv2.bitwise_and(new,new,mask=red)
 
-      cv2.imshow("white",white)
-      cv2.imshow("gold",gold)
+      idk = Find_Possible(new)
 
-      cv2.imshow("new",new)
-      cv2.imshow("maybe",maybe)
-      cv2.imshow("mask",mask)
+      Find_Numbers(idk,image)
+
+      cv2.imshow("idk",idk)
       cv2.imshow("image",image)
       cv2.waitKey(0)
 
